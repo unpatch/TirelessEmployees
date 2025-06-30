056ca0bdfe3734760ef647617742e57d9efbe6eb
unpatch <unpatch@proton.me>
2025-06-30 22:31:59 +0330

    initial commit

---
+++ src/Core.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(TirelessEmployees.Core), "TirelessEmployees", "1.0.0", "unpatch", "https://github.com/unpatch/TirelessEmployees")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+[assembly: MelonColor(0, 157, 255, 82)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+
+namespace TirelessEmployees
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("TirelessEmployees initialized.");
+        }
+    }
+}
---
+++ src/DayNeverEnds.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Il2CppScheduleOne.GameTime;
+
+namespace TirelessEmployees
+{
+    [HarmonyPatch(typeof(TimeManager))]
+	public static class DayNeverEnds
+	{
+        [HarmonyPatch(nameof(TimeManager.IsEndOfDay), MethodType.Getter)]
+		[HarmonyPrefix]
+		public static bool IsEndOfDay(ref bool __result)
+		{
+			__result = false;
+			return false;
+		}
+	}
+}
