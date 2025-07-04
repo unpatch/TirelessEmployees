0f743a7f0060b89502126035b8f32aa5fffe5856
unpatch <unpatch@proton.me>
2025-07-04 14:40:23 +0330

    add Mono support

--- src/Core.cs
+++ src/Core.cs
@@ -3,7 +3,13 @@
 [assembly: MelonInfo(typeof(TirelessEmployees.Core), "TirelessEmployees", "1.0.0", "unpatch", "https://github.com/unpatch/TirelessEmployees")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 [assembly: MelonColor(0, 157, 255, 82)]
+
+#if IL2CPP
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+#elif MONO
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+#endif
+
 
 namespace TirelessEmployees
 {
--- src/DayNeverEnds.cs
+++ src/DayNeverEnds.cs
@@ -1,17 +1,23 @@
 ﻿using HarmonyLib;
+
+#if IL2CPP
 using Il2CppScheduleOne.GameTime;
+#elif MONO
+using ScheduleOne.GameTime;
+#endif
+
 
 namespace TirelessEmployees
 {
     [HarmonyPatch(typeof(TimeManager))]
-	public static class DayNeverEnds
-	{
+    public static class DayNeverEnds
+    {
         [HarmonyPatch(nameof(TimeManager.IsEndOfDay), MethodType.Getter)]
-		[HarmonyPrefix]
-		public static bool IsEndOfDay(ref bool __result)
-		{
-			__result = false;
-			return false;
-		}
-	}
+        [HarmonyPrefix]
+        public static bool IsEndOfDay(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
 }
